34be469c8c63e1f91ddfb304bb338ff9cdc4eb46
unknown <nurlanbh@code.edu.az>
2018-02-23 20:31:46 +0400

    console

---
+++ 22.02.18/22.02.18/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._02._18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = 5;
+            Student Hesabla()
+            {
+                return new Student("Samir");
+            }
+            Console.WriteLine(Hesabla().Name);
+        }
+    }
+    class Student
+    {
+        public string Name;
+        public Student(string _name)
+        {
+            Name = _name;
+        }
+    }
+}
+class Student
+{
+    public string Name; 
+    public Student(string _name)
+    {
+        Name = _name;
+    }
+}
